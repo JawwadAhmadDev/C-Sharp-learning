8a681b3ad6b152186578ee050f9bc161b99350b2
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-12-17 02:41:57 +0500

    hello wrold created

---
+++ helloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Please enter your age");
+
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entered age: " + age);
+            
+        }
+    }
+}
