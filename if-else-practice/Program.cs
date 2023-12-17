e8e1febf34473460eb0b3b102ad6115a265821d4
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-12-17 07:59:30 +0500

    1st problem solved

--- if-else-practice/Program.cs
+++ if-else-practice/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ===================== Solution: 1st Problem ======================//
+
+bool hasBoardingPass, securityPassed;
+double luggageWeight;
+
+// taking inputs from user
+Console.Write("Have you a boarding pass? (0 for false / 1 for true): ");
+hasBoardingPass = int.Parse(Console.ReadLine()) == 0 ? false : true;
+
+Console.Write("Please enter you luggage weight: ");
+luggageWeight = double.Parse(Console.ReadLine());
+
+Console.Write("Are you security passed? (0 for false / 1 for true): ");
+securityPassed = int.Parse(Console.ReadLine()) == 0 ? false : true;
+
+
+// applying conditions
+if (hasBoardingPass && luggageWeight < 20 && securityPassed)
+{
+    Console.WriteLine("Boarding Allowed");
+}
+else
+{
+    Console.WriteLine("Boarding not allowed");
+}
