99de59bb26c86b7b080d4fd9cbfe9934d4cb8b1a
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-12-27 18:01:50 +0500

    solution of 1st problem

--- loops-assignments/Program.cs
+++ loops-assignments/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ==================== Solution: 1st Problem ===================== //
+// Question Statement: Find the Smallest Divisor.
+
+int num, smallestDivisor;
+
+Console.Write("Please enter a positive number greater than 1 for which you want to find smallest divisor: ");
+num = Convert.ToInt32(Console.ReadLine());
+smallestDivisor = num;
+
+int i = 2;
+while (i < num)
+{
+    if (num % i == 0)
+    {
+        smallestDivisor = i;
+        break;
+    }
+    i++;
+}
+
+Console.WriteLine("Smallest Divisor of " + num + " is " + smallestDivisor);
