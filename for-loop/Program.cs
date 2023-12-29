7fd4e9ee6a4f6a6684925afd1f3779ce653d509c
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2023-12-29 07:36:50 +0500

    1st problem solved

--- for-loop/Program.cs
+++ for-loop/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// ===================== 1st Problem: solution =================== //
+// Problem Statement: Print a Pattern of Asterisks
+
+// taking input from user.
+Console.Write("Enter the number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+// printing pattern
+for (int i = 1; i <= rows; i++)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
--- for-loop/obj/Debug/net8.0/for-loop.AssemblyInfo.cs
+++ for-loop/obj/Debug/net8.0/for-loop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("for-loop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef024efb657875f7313557ee5730ccedbdf4a18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ee071e903427aba76ae25a3a09ceb3c9b5aa92e")]
 [assembly: System.Reflection.AssemblyProductAttribute("for-loop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("for-loop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
