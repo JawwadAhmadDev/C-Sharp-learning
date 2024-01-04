5a5e06ba6c8e79102ab6f16127562ab5ff070346
JawwadAhmadDev <jawwadahmad.edu@gmail.com>
2024-01-05 01:48:11 +0500

    problem statements added

--- OOP/inheritence/Program.cs
+++ OOP/inheritence/Program.cs
@@ -1,5 +1,6 @@
 ﻿// ===================== 1st Problem: Solution ==================//
-// School System (Student and Teacher Implementations):
+// School System(Student and Teacher Implementations):
+// Design a base class Person with common attributes like name and address. Derive classes such as Student and Teacher from Person, adding specific properties and methods relevant to each.
 
 // Base class
 // public class Person
@@ -55,7 +56,7 @@
 
 // ===================== 2nd Problem: Solution ==================//
 // Zoo Animal Hierarchy:
-
+// Create a base class Animal with general characteristics like age and weight. Extend this class to specific animals like Lion, Elephant, and Giraffe, each with unique attributes and behaviors.
 
 // Base class
 public class Animal
@@ -102,6 +103,7 @@ public class Lion : Animal
 
 // ===================== 3rd Problem: Solution ==================//
 // Company Hierarchy:
+// Implement a base class Employee with common fields like employee ID and department. Derive different roles like Manager, Engineer, and SalesRepresentative, adding role-specific methods and properties.
 
 // Base class
 // public class Employee
@@ -147,7 +149,8 @@ public class Lion : Animal
 
 // ===================== 4th Problem: Solution ==================//
 // Transportation System:
-
+// Develop a base class TransportMode with common features like maximum speed and capacity. Extend this class into specific transportation modes like Bus, Train, and Airplane, each with unique attributes.
+// Create a base class ElectronicDevice with basic functionalities like powerOn() and powerOff(). Extend this class with specific products like Television, Radio, and Laptop, each having additional features and functionalities.
 
 // Base class
 public class TransportMode
